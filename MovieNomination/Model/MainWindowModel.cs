c98003bb91c108861577ee5c624c59fd945d8083
jona339e <jonasoe@hotmail.com>
2023-01-24 12:31:10 +0100

    Add project files.

---
+++ MovieNomination/Model/MainWindowModel.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MovieNomination.Model
+{
+    class MainWindowModel
+    {
+        // Database Depiction
+        public string movieTitle { get; set; }
+        public string directorName { get; set; }
+        public DateTime releaseDate { get; set; }
+        public int rating { get; set; }
+
+
+        // Business Logic
+
+        public void SqlInsert()
+        {
+            const string connectionString = @"Data Source=192.168.99.60, 1433;
+                                            Initial Catalog=MovieNominationDB;
+                                            User ID=WPFLogin;Password=Passw0rd;
+                                            encrypt=false;";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    SqlCommand cmd = new("INSERT INTO MOVIE VALUES(@movieTitle, @directorName, @releaseDate, @rating)", connection);
+                    cmd.Parameters.AddWithValue("@movieTitle", movieTitle);
+                    cmd.Parameters.AddWithValue("@directorName", directorName);
+                    cmd.Parameters.AddWithValue("@releaseDate", releaseDate);
+                    cmd.Parameters.AddWithValue("@rating", rating);
+
+                    SqlDataAdapter da = new();
+                    da.InsertCommand = cmd;
+                    da.InsertCommand.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+    }
+}
---
+++ MovieNomination/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,78 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MovieNomination.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MovieNomination.ViewModel
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public string movieTitle;
+
+        [ObservableProperty]
+        public string movieDirector;
+
+        [ObservableProperty]
+        public DateTime releaseDate = DateTime.Now;
+
+        [ObservableProperty]
+        public int selectedRating = 1;
+
+        [ObservableProperty]
+        public List<int> ratingList = new() { 1, 2, 3, 4, 5, 6};
+
+
+
+        // Application Logic
+
+        [RelayCommand]
+        public void OnConfirm()
+        {
+            if (ValidationCheck())
+            {
+                MainWindowModel model = new MainWindowModel
+                {
+                    movieTitle = MovieTitle,
+                    directorName = MovieDirector,
+                    releaseDate = ReleaseDate,
+                    rating = SelectedRating
+
+                };
+                model.SqlInsert();
+            }
+            else
+            {
+                MessageBox.Show("Venligst udfyld alle data.");
+            }
+        }
+
+        private bool ValidationCheck()
+        {
+            if (movieTitle == string.Empty || movieDirector == string.Empty)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        [RelayCommand]
+        public void OnCancel()
+        {
+            MovieTitle = string.Empty;
+            MovieDirector = string.Empty;
+            ReleaseDate = DateTime.Now;
+            SelectedRating = RatingList[0];
+        }
+
+
+    }
+}
